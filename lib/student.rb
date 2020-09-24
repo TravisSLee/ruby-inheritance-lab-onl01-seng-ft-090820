1e9ca9d74125e19f2ea1d68031a8c3cd17cb419b
Travis Lee <travisslee223@gmail.com>
2020-09-24 04:11:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 class Student < User
 
-attr_accessor :knowledge
-
-def initialize
-  @knowledge = []
-end
-
-def learn(string)
-  @knowledge << string
-end
-
-def knowledge
-  @knowledge
-end
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    @knowledge
+  end
 end
